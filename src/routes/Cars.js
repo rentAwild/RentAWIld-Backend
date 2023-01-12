6e33a14cbaf68745e2eadfb6ab3652f89cbf550e
SIMON THOUET <simon.thouet@gmail.com>
2023-01-12 18:27:10 +0000

    Correct wrong merge actions

--- src/routes/Cars.js
+++ src/routes/Cars.js
@@ -10,9 +10,4 @@ CarsRouter.get("/Cars/:id", Cars.retrieveCarById);
 CarsRouter.patch("/Cars/:id", Cars.updateCarKilometer);
 // CarsRouter.patch("/Cars/:id", Cars.changeMaintenance);
 
-CarsRouter.post("/Cars/:id/book", isTheCarBooked, Cars.bookCar);
-
-CarsRouter.post("/Cars", Cars.createCar);
-CarsRouter.delete("/Cars/:id", Cars.deleteCar);
-
 module.exports = CarsRouter;
