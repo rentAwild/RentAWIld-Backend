3315908895d5d50970b50bf6d330abec84285d43
André Macedo <asilvamacedo@gmail.com>
2023-01-12 23:30:29 +0000

    created route to remove company

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -4,6 +4,6 @@ const Users = require("../controllers/Users");
 
 UsersRouter.get("/users/type", Users.retrieveUserByEmail);
 UsersRouter.get("/users", Users.retrieveUsers);
-UsersRouter.delete("/users/:id", Users.retrieveUsers);
+UsersRouter.delete("/users/:id", Users.removeCompany);
 
 module.exports = UsersRouter;
